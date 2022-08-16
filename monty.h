a79fa3d6de0b96577f4e7e7aba99c0826062eeba
Cygnus-8 <muchrismonfort@gmail.com>
2022-08-16 05:47:31 -0700

    Monty.h adding features

--- monty.h
+++ monty.h
@@ -1,6 +1,12 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+void read_line(char *file_name);
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -30,5 +36,4 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
 #endif
