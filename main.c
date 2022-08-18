81e3d168c1cd2724388613212ef74b3304c0c31a
mathys <mathys.laurent3164@gmail.com>
2022-08-18 07:13:16 -0700

    fix global var in main

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 #include "monty.h"
 
+stack_t *head = NULL;
+
 /**
  * main - Entry Point
  * @argc: Number of command line arguments.
  * @argv: An array containing the arguments.
  * Return: Always Zero.
  */
-
-stack_t *head = NULL;
-
 int main(int argc, char **argv)
 {
 	if (argc < 2 || argc > 2)
