794f0d64846417f4df27da0e2ae76743c4832086
Cygnus-8 <muchrismonfort@gmail.com>
2022-08-18 05:43:08 -0700

    All bugs on task 0 fixed

--- Stack_operation.c
+++ Stack_operation.c
@@ -39,7 +39,7 @@ void print_stack(stack_t **stack, unsigned int line_number)
 	if (stack == NULL)
 		exit(EXIT_FAILURE);
 	tmp = *stack;
-	while (stack != NULL)
+	while (tmp != NULL)
 	{
 		printf("%d\n", tmp->n);
 		tmp = tmp->next;
