cfcadb58f7a7f0d80e066b638f3e761463b8ee57
Cygnus-8 <muchrismonfort@gmail.com>
2022-08-18 04:01:43 -0700

    Task 1 features added

--- monty.h
+++ monty.h
@@ -1,6 +1,22 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
+
+void open_file(char *file_name);
+void read_line(FILE *fd);
+int line_interpreter(char *lineptr, int line_number, int data_mode);
+void error(int error_code, ...);
+void search_func(char *opcode, char *value, int line_number, int data_mode);
+void exec_fun(opcode_func f, char *opcode, char *value, int line_number, int data_mode);
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -12,9 +28,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -27,8 +43,15 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+extern stack_t *head;
+stack_t *create_node(int n);
+void free_nodes(void);
+
+void add_to_stack(stack_t **node_to_add, __attribute__((unused))unsigned int line_number);
+void print_stack(stack_t stack, unsigned int line_number);
+typedef void (*opcode_func)(stack_t **, unsigned int);
 #endif
